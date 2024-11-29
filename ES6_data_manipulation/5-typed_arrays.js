1857e3751cb005e5a21cbf16cd8d29be0f18a773
ruslan200677 <qaqli20033@gmail.com>
2024-11-29 19:16:15 +0400

    add

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,14 +1,14 @@
 function createInt8TypedArray(length, position, value) {
-  const buffer = new ArrayBuffer(length);
-  const view = new DataView(buffer, 0);
-
-  if (position > length - 1) {
-    throw Error('Position outside range');
+    const buffer = new ArrayBuffer(length);
+    const view = new DataView(buffer, 0);
+  
+    if (position > length - 1) {
+      throw Error('Position outside range');
+    }
+  
+    view.setInt8(position, value);
+  
+    return view;
   }
-
-  view.setInt8(position, value);
-
-  return view;
-}
-
-export default createInt8TypedArray;
+  
+  export default createInt8TypedArray;
