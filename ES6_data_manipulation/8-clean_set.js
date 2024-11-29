ba743b97b93624785aa659de3ad6de0af69fe51e
ruslan200677 <qaqli20033@gmail.com>
2024-11-29 19:39:23 +0400

    gs

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,7 @@
-
 export default function cleanSet(set, startString) {
-    if (typeof startString !== 'string' || startString === '') return '';
-    return [...set].reduce((acc, currentIndex) => {
-      if (currentIndex.startsWith(startString)) return acc.concat(currentIndex.slice(startString.length), '-');
-      return acc;
-    }, '').slice(0, -1);
-  }
+  if (typeof startString !== 'string' || startString === '') return '';
+  return [...set].reduce((acc, currentIndex) => {
+    if (currentIndex.startsWith(startString)) return acc.concat(currentIndex.slice(startString.length), '-');
+    return acc;
+  }, '').slice(0, -1);
+}
