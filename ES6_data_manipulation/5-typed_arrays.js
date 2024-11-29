039a063823c7fd1fafb8a3c749ce28bf7fd79cc6
ruslan200677 <qaqli20033@gmail.com>
2024-11-29 19:15:25 +0400

    fix

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,10 +1,14 @@
-export default function createInt8TypedArray(length, position, value) {
-  const arrBuff = new ArrayBuffer(length);
-  const int8view = new Int8Array(arrBuff);
-  if (position > length) {
+function createInt8TypedArray(length, position, value) {
+  const buffer = new ArrayBuffer(length);
+  const view = new DataView(buffer, 0);
+
+  if (position > length - 1) {
     throw Error('Position outside range');
-  } else {
-    int8view[position] = value;
   }
-  return new DataView(arrBuff);
+
+  view.setInt8(position, value);
+
+  return view;
 }
+
+export default createInt8TypedArray;
