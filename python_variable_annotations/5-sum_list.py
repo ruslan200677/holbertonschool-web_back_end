eb4517639936092e4b57298fa25f5164be6b7d11
ruslan200677 <qaqli20033@gmail.com>
2024-12-08 13:57:00 +0000

    5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+                                                     ~#!/usr/bin/env python3
+"""Complex types - list of floats"""
+import typing
+
+
+def sum_list(input_list: typing.List[float]) -> float:
+    """Complex types - list of floats"""
+    return sum(input_list)
