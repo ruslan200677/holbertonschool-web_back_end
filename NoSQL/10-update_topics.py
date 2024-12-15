f8c3cb0251a4e28e66fe63e0aed107b6e6580d33
ruslan200677 <qaqli20033@gmail.com>
2024-12-15 15:27:16 +0000

    fixx

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module containing functions for updating topics."""
+
+
+def update_topics(mongo_collection, name, topics):
+    """Updates topics in MongoDB."""
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
+    
